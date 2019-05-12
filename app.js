08a0191375633418c16dff8fae7075023410fa06
Ilyas Ezzahid <ezzil1997@gmail.com>
2019-05-12 20:35:23 +0700

    get the balance of ether of the used account in Ganache and MetaMask

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const assert = require('assert');
 const Web3 = require('web3');
 const ganache = require('ganache-cli');
-const web3 = new Web3(ganache.provider());
+const rpcURL = 'HTTP://127.0.0.1:7545'
+const web3 = new Web3(rpcURL);
 const address = '0x58145DA63490CD9213FdE2055060d582e7D7A67A';
 
 let accounts;
     
 async function init(){
+// get All the accounts in Ganache
     accounts = await web3.eth.getAccounts();
-    console.log(accounts);
+    //console.log(accounts);
+    web3.eth.getAccounts(console.log);
 }
-init();
+
+web3.eth.getBalance(address, (err, wei) => {
+    balance = web3.utils.fromWei(wei, 'ether')
+    console.log(balance);
+});
